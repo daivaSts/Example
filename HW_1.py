ed4d79f9f090aa033306d1df5d6737c691b1c6d6
daivaSts <daiva.satas@gmail.com>
2014-01-06 08:49:14 -0600

    PL_HW Fsm Simulator
    Final

--- HW_1.py
+++ HW_1.py
@@ -6,26 +6,18 @@ edges = {(1, 'a') : 2,
 accepting = [3]
 def fsmsim(string, current, edges, accepting):
     if string == "":
-        print "current in accepting"
-        return True
+        return current in accepting
+        
     else:
         letter = string[0]
-       
-    if edges[current, letter] in edges.values():
-        current  = edges[current, letter]
-        string = string[1:]
-        fsmsim(string, current, edges, accepting)
-        if edges[current, letter] == accepting:
-            print "accepting state"
-            return True
-    else:
-        return False
 
-        # QUIZ: You fill this out!
-        # Is there a valid edge? 
-        # If so, take it.
-        # If not, return False.
-        # Hint: recursion.
+        if (current, letter) in edges:
+            current  = edges[current, letter]
+            string = string[1:]
+            return fsmsim(string, current, edges, accepting)
+            
+        else:
+            return False
+
 
 print fsmsim("aaa111",1,edges,accepting)
-# >>> True
