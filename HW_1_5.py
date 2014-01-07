e4801efb3ae6bc93d0c4b74d66ed6faac637084b
daivaSts <daiva.satas@gmail.com>
2014-01-07 10:04:10 -0600

    HW_1_5
    changes

--- HW_1_5.py
+++ HW_1_5.py
@@ -1,29 +1,21 @@
-edges = { (1, 'a') : [2, 3],
-          (2, 'a') : [2],
-          (3, 'b') : [4, 2],
-          (4, 'c') : [5] }
-accepting = [5] #'abc'
-edges2 = { (1, 'a') : [1],
-           (2, 'a') : [2] }
-accepting2 = [2] #'none'
-
+Python 3.3.3 (v3.3.3:c3896275c0f6, Nov 18 2013, 21:18:40) [MSC v.1600 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
 def nfsmaccepts(current, edges, accepting, visited): 
-     
-    if current in accepting:  
-        return '' 
-    
+    word = ""
+    if current in visited:
+        return None
+    if current in accepting:        
+        return ''
     else:
-        for edge in edges: 
-            visited.append(edge[0]) 
+        visited.append(current)
+        for edge, destination_state in edges.items():             
+            state, letter = edge        
+            if state == current:
+                for destination in destination_state:
+                    current =  destination
+                    nfsmaccepts(current, edges, accepting, visited)
+                    return word + letter   
+        else:
         
-        if (current, letter) in edges:
-            destinations = edges[current, letter]
-            new_string = string[1:]
-            for destination in destinations:                
-                if nfsmsim(new_string,destination,edges,accepting):
-                    return True
-        
-        return 'z'
-  
-print nfsmaccepts(1, edges, accepting, [])    
- 
+            return None
