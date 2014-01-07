5ca7bf0a1d10587d4d8f7affbf58029d571952ff
daivaSts <daiva.satas@gmail.com>
2014-01-07 10:06:36 -0600

    Almost there

--- HW_1_5.py
+++ HW_1_5.py
@@ -1,6 +1,4 @@
-Python 3.3.3 (v3.3.3:c3896275c0f6, Nov 18 2013, 21:18:40) [MSC v.1600 32 bit (Intel)] on win32
-Type "copyright", "credits" or "license()" for more information.
->>> 
+
 def nfsmaccepts(current, edges, accepting, visited): 
     word = ""
     if current in visited:
